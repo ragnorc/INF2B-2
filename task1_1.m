37169f0da20b37f9ebed68966cb47344ac7c7b54
Ragnor Comerford <hello@ragnor.co>
2019-03-29 02:47:46 +0000

    Clean up task 1.1

--- task1_1.m
+++ task1_1.m
@@ -4,21 +4,19 @@ function task1_1(X, Y)
 % Input:
 %  X : M-by-D data matrix (double)
 %  Y : M-by-1 label vector (unit8)
-images = zeros(10,784,10);
+
 indices = ones([1,10]);
-montageVar = zeros(28,28,10);
-  % 28x28 image x 10 samples x to digits
-% 10 samples * 784 pixels (image row vector) * 10 classes
+montageImages = zeros(28,28,10,10);
+% 4D matrix 28x28x10x10
 for i=1:size(Y,1)
     classIndex = Y(i)+1;
     sampleIndex = indices(classIndex);
     if sampleIndex <= 10
-    images(sampleIndex,:,classIndex) = X(i,:);
-    montageVar(:,:,sampleIndex,classIndex) = reshape(X(i,:), 28, 28)';
+    montageImages(:,:,sampleIndex,classIndex) = reshape(X(i,:), 28, 28)';
     indices(classIndex) = sampleIndex + 1;
     end
 end
 for c=1:10  
-figure, montage(montageVar(:,:,:,c));
+figure, montage(montageImages(:,:,:,c));
 end
 end
