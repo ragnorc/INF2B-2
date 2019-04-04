4a8cfc78bc5c6ea6adc4f9f485b2e85c61141d65
Ragnor Comerford <hello@ragnor.co>
2019-04-05 01:23:08 +0200

    Implements 2.8 and fix 2.5

--- run_mgcs.m
+++ run_mgcs.m
@@ -11,4 +11,37 @@ function [Ypreds, MMs, MCovs] = run_mgcs(Xtrain, Ytrain, Xtest, epsilon, L)
 %  MCovs   : (L*K)-by-D-by-D 3D array of covariance matrices (double)
 
 
+numClasses = 10;
+[numTrain, dimTrain] = size(Xtrain);
+[numTest, dimTest] = size(Xtest);
+Ypreds = zeros(size(Xtest,1),1);
+MMs = zeros(numClasses*L,dimTrain);
+MCovs = zeros(numClasses*L,dimTrain,dimTrain);
+
+%Learn parameters
+clusIndex = 0;
+for i=1:numClasses
+    classElements = Xtrain(find(Ytrain==(i-1)),:);
+    [C, idx, SSE] = my_kMeansClustering(classElements, L, classElements(1:L,:));
+     
+    for clus=1:L
+        MMs(clusIndex+clus,:) = C(clus,:);
+        classClusterElements = classElements(idx==clus);
+        MCovs(clusIndex+clus,:,:) = myCov(classClusterElements);
+    end
+    clusIndex = clusIndex + L;
+end
+
+
+%Classification
+logProbMatrix = zeros(numClasses*L,numTest);
+for i=1:numClasses*L
+   logProbMatrix(i,:) = myLogpdf(MMs(i,:),reshape(MCovs(i,:,:), [dimTrain,dimTrain]), Xtest, epsilon);
+ end
+
+[maxV,maxI] = max(logProbMatrix);
+Ypreds = (ceil(maxI / L) - ones(1,numTest))'; %Subtract 1 since working with labels zero
+
+
+
 end
--- task2_8.m
+++ task2_8.m
@@ -1,4 +1,4 @@
-function = task2_8(Xtrain, Ytrain, Xtest, Ytest, epsilon, L)
+function task2_8(Xtrain, Ytrain, Xtest, Ytest, epsilon, L)
 % Input:
 %   Xtrain : M-by-D training data matrix (double)
 %   Xtrain : M-by-1 label vector (uint8) for Xtrain
@@ -6,6 +6,25 @@ function = task2_8(Xtrain, Ytrain, Xtest, Ytest, epsilon, L)
 %   Ytest  : N-by-1 label vector (uint8) for Xtest
 %   epsilon : A scalar parameter for regularisation
 %   L      : scalar (integer) of the number of Gaussian distributions per class
+
+
+numSamples = size(Xtest,1);
+
+tic
+[Ypreds, MMs, MCovs] = run_mgcs(Xtrain, Ytrain, Xtest, epsilon, L);
+time = toc;
+fprintf('Number of test samples: %d\n',numSamples);
+fprintf('User time taken in seconds: %.2f\n',time);
+[cm, acc] = comp_confmat(Ytest,Ypreds(:,1));
+fprintf('Accuracy: %.4f\n',acc);
+fprintf('Number of wrongly classified test samples: %d\n',numSamples-sum(diag(cm)));
+save(sprintf('task2_8_cm_%d.mat',L),'cm');
+Ms1 = MMs(1:L,:);
+Covs1 = MCovs(1:L,:,:);
+
+save(sprintf('task2_8_g%d_m1.mat',L),'Ms1');
+save(sprintf('task2_8_g%d_cov1.mat',L),'Covs1');
+
 
 
 end
