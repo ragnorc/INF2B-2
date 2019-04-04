07fd985b48cb4a849743e690048fb9205ea8c588
Ragnor Comerford <hello@ragnor.co>
2019-04-04 23:25:11 +0200

    Finish task 2.7

--- task2_7.m
+++ task2_7.m
@@ -11,4 +11,8 @@ function [CM, acc] = task2_7(Xtrain, Ytrain, Xtest, Ytest, epsilon, ratio)
 %  CM     : K-by-K matrix (integer) of confusion matrix
 %  acc    : scalar (double) of correct classification rate
 
+numSamples = size(Xtrain,1);
+subset = floor(ratio*numSamples);
+[Ypreds, Ms, Covs] = run_gaussian_classifiers(Xtrain(1:subset,:), Ytrain(1:subset,:), Xtest, epsilon);
+[CM, acc] = comp_confmat(Ytest,Ypreds(:,1));
 end
